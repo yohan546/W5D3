a11b631bd6d1939fd2e1d7a669e1d9171d349fc6
yohan oh <yohan.oh332@gmail.com>
2021-01-27 16:53:18 -0500

    finished tables

--- AA_questions/import_db.sql
+++ AA_questions/import_db.sql
@@ -2,7 +2,11 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS question_follows;
 DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS questions_likes;
+
 PRAGMA foreign_keys = ON;
+
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL, 
@@ -12,7 +16,7 @@ CREATE TABLE users (
 INSERT INTO
     users (fname, lname)
 VALUES
-    ("yohan", "oh"), ("dan","blonc"), ("random", "guy");
+    ("yohan", "oh"), ("dan","blonc"), ("john", "park");
 
 
 CREATE TABLE questions (
@@ -32,14 +36,20 @@ VALUES
         ("questions number 1"),
         (SELECT id FROM users WHERE fname = 'yohan')
 
-    ),
-    
-        (
+    ),(
+
         (SELECT 'dans question' FROM users WHERE fname = "dan"),
         ("questions number 2"),
         (SELECT id FROM users WHERE fname = "dan")
 
+    ), (
+
+        (SELECT 'johns question' FROM users WHERE fname = "john"),
+        ("questions number 3"),
+        (SELECT id FROM users WHERE fname = "john")
+
     );
+
 
 
 CREATE TABLE question_follows (
@@ -58,12 +68,16 @@ VALUES
        (SELECT id FROM users WHERE fname = "yohan"),
        (SELECT id FROM questions WHERE title = "yohans question")
 
-    ),
-    (
+    ),(
     
         (SELECT id FROM users WHERE fname = "dan"),
        (SELECT id FROM questions WHERE title = "dans question")
-        
+ 
+    ),(
+    
+        (SELECT id FROM users WHERE fname = "john"),
+       (SELECT id FROM questions WHERE title = "johns question")
+
     );
 
     CREATE TABLE replies(
@@ -98,12 +112,101 @@ VALUES
         (Select id FROM users WHERE fname = "dan"),
         ("Reply child")
     );
+
     INSERT INTO 
         replies (questions_id, parent_reply_id, users_id, body)
     VALUES
     (
         (SELECT id FROM questions WHERE title = "yohans question"),
         (SELECT id FROM replies WHERE body = "Reply child"),
-        (Select id FROM users WHERE fname = "random"),
+        (Select id FROM users WHERE fname = "john"),
         ("Reply child child")
     );
+
+
+
+CREATE TABLE questions_likes(
+    id INTEGER PRIMARY KEY,
+    questions_id INTEGER NOT NULL,
+    num_of_likes INTEGER DEFAULT 0,
+    users_id INTEGER NOT NULL,
+    
+    FOREIGN KEY (questions_id) REFERENCES questions(id)
+    FOREIGN KEY (users_id) REFERENCES users(id)
+    );
+
+    INSERT INTO 
+        questions_likes (questions_id, users_id)
+    VALUES 
+    (
+
+        (SELECT id FROM questions WHERE title = "yohans question"),
+        
+        (SELECT id FROM users WHERE fname = "yohan")
+
+    );
+
+    INSERT INTO 
+        questions_likes (questions_id, users_id)
+    VALUES 
+    (
+
+        (SELECT id FROM questions WHERE title = "dans question"),
+        
+        (SELECT id FROM users WHERE fname = "dan")
+
+    );
+
+    INSERT INTO 
+        questions_likes (questions_id, users_id)
+    VALUES 
+    (
+
+        (SELECT id FROM questions WHERE title = "johns question"),
+        
+        (SELECT id FROM users WHERE fname = "john")
+
+    );
+
+
+
+
+    INSERT INTO 
+        questions_likes (questions_id, num_of_likes, users_id)
+    VALUES 
+    (
+
+        (SELECT id FROM questions WHERE title = "dans question"),
+        (SELECT num_of_likes + 1  FROM questions_likes WHERE id = 2),
+        (SELECT id FROM users WHERE fname = "yohan")
+
+    );
+
+    INSERT INTO 
+        questions_likes (questions_id, num_of_likes, users_id)
+    VALUES 
+    (
+
+        (SELECT id FROM questions WHERE title = "yohans question"),
+        (SELECT num_of_likes + 1  FROM questions_likes WHERE id = 1),
+        (SELECT id FROM users WHERE fname = "dan")
+
+    );
+
+    INSERT INTO 
+        questions_likes (questions_id, num_of_likes, users_id)
+    VALUES 
+    (
+
+        (SELECT id FROM questions WHERE title = "yohans question"),
+        (SELECT num_of_likes + 1  FROM questions_likes WHERE id = 5),
+        (SELECT id FROM users WHERE fname = "john")
+
+    );
+
+
+
+
+
+
+
